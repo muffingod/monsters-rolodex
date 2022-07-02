6aeb825dc184fc9d9cafdd3f9c4944fbf6989d17
Kristóf Vogyák <muffingod001@gmail.com>
2022-07-02 13:48:50 +0200

    Adding files for gh-pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import "./search-box.styles.css";
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input
+    className="search"
+    type="search"
+    onChange={handleChange}
+    placeholder={placeholder}
+  />
+);
